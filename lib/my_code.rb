07d2a2570ca503c327c3734f86e1ad939fe39f15
Karl Matthes <karlmatthes@gmail.com>
2019-11-07 17:39:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,4 +1,4 @@
-#Mapping
+#Mapping methods
 def map_to_negativize(source_array)
   output_array = []
   source_array.length.times do |index|
@@ -29,4 +29,17 @@ def map_to_square(source_array)
     output_array.push(source_array[index] ** 2)
   end
   return output_array
+end
+
+#Reducing methods
+def reduce_to_total(source_array, starting_point)
+  
+end
+
+def reduce_to_all(source_array)
+  
+end
+
+def reduce_to_any_true(source_array)
+  
 end
