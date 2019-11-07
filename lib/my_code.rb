2fa90cc430d16ff81a1993abe411b96340c3273a
Karl Matthes <karlmatthes@gmail.com>
2019-11-07 17:37:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
-# My Code here....
+#Mapping
+def map_to_negativize(source_array)
+  output_array = []
+  source_array.length.times do |index|
+    output_array.push(source_array[index] * -1)
+  end
+end
+
+def map_to_no_change(source_array)
+  output_array = []
+  source_array.length.times do |index|
+    output_array.push(source_array[index])
+  end
+end
+
+def map_to_double(source_array)
+  output_array = []
+  source_array.length.times do |index|
+    output_array.push(source_array[index] * 2)
+  end
+end
+
+def map_to_square(source_array)
+  output_array = []
+  source_array.length.times do |index|
+    output_array.push(source_array[index] ** 2)
+  end
+end
